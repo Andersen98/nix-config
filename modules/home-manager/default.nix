6f1937cc6965ed1d8518cf6065febaf0d327f019
Hannah Andersen <thehumanme@gmail.com>
2024-03-21 20:35:51 -0600

    modified config to split up home manager into modules

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,3 +1,7 @@
 {
   starship-pastel-preset = import ./starship-pastel-preset.nix;
+  neovim = import ./neovim.nix;
+  fish = import ./fish.nix;
+  kitty = import ./kitty.nix;
+  bash = import ./bash.nix;
 }
